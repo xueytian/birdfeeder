d2233b9091795b4a26822cc50632b6d78ad08889
xueytian <xueytian@gmail.com>
2021-03-31 13:25:36 +0800

    add copy to the read list

--- Feeder_App/views.py
+++ Feeder_App/views.py
@@ -2,15 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import image, food
+import copy
 
 def index(request):
     pic_list = image.objects.all()
     return render(request, 'pic.html', {'pic_list': pic_list})
 
 def message(request):
-    message_list_unread = food.objects.filter(check_view=0).order_by('-notice_date')
-    message_list_read = food.objects.filter(check_view=1).order_by('-notice_date')
+    message_list_unread = food.objects.filter(check_view=False).order_by('-notice_date')
+    message_list_read = food.objects.filter(check_view=True).order_by('-notice_date')
+    message_list = copy.copy(message_list_read)
+    print(message_list_read)
     for message in message_list_unread:
-        message.check_view = 1
+        message.check_view = True
         message.save()
-    return render(request, 'message.html', {'message_list_unread': message_list_unread, 'message_list_read': message_list_read})
+    print(message_list)
+    return render(request, 'message.html', {'message_list_unread': message_list_unread, 'message_list_read': message_list})
--- SSPBF/settings.py
+++ SSPBF/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'SSPBF.wsgi.application'
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'sspbf',
+#         'NAME': 'sspbf_1',
 #         'USER':'root',
 #         'PASSWORD':'',
 #         'HOST':'localhost',
